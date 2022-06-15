f5af6b88642ab31cac73225deb2f13488ffdde1d
nair0lf32 <florianedem@gmail.com>
2022-06-15 17:47:28 +0100

    doing task 0

--- monty.c
+++ monty.c
@@ -1,5 +1,24 @@
 #include "monty.h"
 /**
+*main - entry point
+*@argc: argument count
+*@argv: array of arguments
+*Return: EXIT_SUCCESS or EXIT_FAILURE!!!
+**/
+int main(int argc, char **argv)
+{
+stack_t *head;
+stack_init(&head);
+if (argc != 2)
+{
+printf("USAGE: monty file\n");
+exit(EXIT_FAILURE);
+} else {
+read_file(argv[1], &head);
+exit(EXIT_SUCCESS);
+}
+}
+/**
 *stack_init - initialize stack
 *@head: pointer to pointer to stack_t 
 *Return: void
@@ -114,24 +133,5 @@ while (temp != NULL)
 {
 printf("%d\n", temp->n);
 temp = temp->next;
-}
-}
-/**
-*main - entry point
-*@argc: argument count
-*@argv: array of arguments
-*Return: EXIT_SUCCESS or EXIT_FAILURE!!!
-**/
-int main(int argc, char **argv)
-{
-stack_t *head;
-stack_init(&head);
-if (argc != 2)
-{
-printf("USAGE: monty file\n");
-exit(EXIT_FAILURE);
-} else {
-read_file(argv[1], &head);
-exit(EXIT_SUCCESS);
 }
 }
